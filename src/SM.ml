e2ea3b448812801f1ac4ebc164287c2e29878ece
Nikita Vorobyev <vorobyev.nikita@protonmail.com>
2019-02-25 17:25:24 +0300

    second done

--- src/SM.ml
+++ src/SM.ml
@@ -23,7 +23,25 @@ type config = int list * Syntax.Stmt.config
 
    Takes a configuration and a program, and returns a configuration as a result
  *)                         
-let eval _ = failwith "Not yet implemented"
+let hd_tl = Syntax.Stmt.hd_tl
+let rec eval (st, (s, i, o)) p = 
+    let eval_expr expr = match expr with
+        | BINOP op -> (match st with
+            | (y::x::xs) -> (Syntax.Expr.str_to_op op x y :: xs, (s, i, o)) 
+            | _ -> failwith "Stack is empty on binop")
+        | CONST x -> (x :: st, (s, i, o))
+        | READ -> let (head, tail) = hd_tl i "Unexpected end of input" in
+                      (head :: st, (s, tail, o))
+        | WRITE -> let (head, tail) = hd_tl st "Stack is empty on write" in
+                       (tail, (s, i, head :: o))
+        | LD name -> (s name :: st, (s, i, o))
+        | ST name -> let (head, tail) = hd_tl st "Stack is empty on store" in
+                     let new_state = Syntax.Expr.update name head s in
+                     (tail, (new_state, i, o)) in
+    match p with
+        | x::xs -> eval (eval_expr x) xs
+        | _ -> (st, (s, i, o)) 
+
 
 (* Top-level evaluation
 
@@ -41,4 +59,13 @@ let run i p = let (_, (_, _, o)) = eval ([], (Syntax.Expr.empty, i, [])) p in o
    stack machine
  *)
 
-let compile _ = failwith "Not yet implemented"
+let rec compile_expr e = match e with
+    | Syntax.Expr.Const x -> [CONST x]
+    | Syntax.Expr.Var n -> [LD n]
+    | Syntax.Expr.Binop (op, e1, e2) -> compile_expr e1 @ compile_expr e2 @ [BINOP op]
+
+let rec compile p = match p with
+    | Syntax.Stmt.Read name -> [READ; ST name]
+    | Syntax.Stmt.Write expr -> compile_expr expr @ [WRITE]
+    | Syntax.Stmt.Assign (name, expr) -> compile_expr expr @ [ST name]
+    | Syntax.Stmt.Seq (e1, e2) -> compile e1 @ compile e2
--- src/Syntax.ml
+++ src/Syntax.ml
@@ -41,7 +41,35 @@ module Expr =
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let bool_to_int x = if x then 1 else 0
+    let int_to_bool x = if x == 0 then false else true
+
+    let boolop_with_ret_to_int op = fun lhs rhs -> bool_to_int (op (int_to_bool lhs) (int_to_bool rhs))
+    let boolop_to_int op = fun lhs rhs -> bool_to_int (op lhs rhs)
+
+    let str_to_op op = match op with
+        | "+" -> ( + )
+        | "-" -> ( - )
+        | "*" -> ( * )
+        | "/" -> ( / )
+        | "%" -> ( mod )
+        | "!!" -> boolop_with_ret_to_int ( || )
+        | "&&" -> boolop_with_ret_to_int ( && )
+        | "==" -> boolop_to_int ( == )
+        | "!=" -> boolop_to_int ( != )
+        | "<=" -> boolop_to_int ( <= )
+        | "<" -> boolop_to_int ( < )
+        | ">=" -> boolop_to_int ( >= )
+        | ">" -> boolop_to_int ( > )
+        | _ -> failwith "unsupported op"
+
+
+    let rec eval s e = match e with
+        | Const x -> x
+        | Var n -> s n
+        | Binop (op, e1, e2) -> let r1 = eval s e1 in
+                                let r2 = eval s e2 in
+                                str_to_op op r1 r2
 
   end
                     
@@ -65,7 +93,16 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+    let hd_tl l msg = match l with
+        | head::tail -> (head, tail)
+        | _ -> failwith(msg)
+    let rec eval (s, i, o) p = match p with
+        | Read name -> let (head, tail) = hd_tl i "Unexpected end of input" in
+                       (Expr.update name head s, tail, o)
+        | Write e -> (s, i, Expr.eval s e :: o)
+        | Assign (name, e) -> (Expr.update name (Expr.eval s e) s, i, o)
+        | Seq (e1, e2) -> let (s1, i1, o1) = eval (s, i, o) e1 in
+                         eval (s1, i1, o1) e2
                                                          
   end
 
